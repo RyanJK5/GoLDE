91174974f2205022ebf7f0124e884a93be155db7
Ryan <ryan.keane051@gmail.com>
2026-04-03 23:11:22 -0400

    [CHANGE] Prettied RuleWidget

--- GOLGuiLib/src/widgets/RuleWidget.cpp
+++ GOLGuiLib/src/widgets/RuleWidget.cpp
@@ -12,15 +12,24 @@ RuleWidget::RuleWidget()
 
 WidgetResult RuleWidget::UpdateImpl(const EditorResult& state) {
     ImGui::PushStyleVarY(ImGuiStyleVar_FramePadding, 10.f);
+    ImGui::Text("Rule");
+
     bool success = ImGui::InputTextWithHint(
         "##RuleWidgetInput", "Enter Rule...", &m_InputText,
         ImGuiInputTextFlags_EnterReturnsTrue);
+    ImGui::SetItemTooltip(
+        "Enter any outer-totalistic rule, which is defined as "
+        "\"B1...8/S0...8\". The digits after 'B'\n"
+        "specify the number of neighbors a cell can have to be born, and the "
+        "digits after 'S'\n"
+        "specify the number of neighbors a cell can have to survive to the "
+        "next generation.\n"
+        "Conway's Game of Life is therefore defined as B3/S23.");
 
     ImGui::SameLine();
     ImGui::PushStyleVarY(ImGuiStyleVar_ItemSpacing, 30.f);
 
     ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
-
     {
         DisabledScope disableIf{!Actions::Editable(state.Simulation.State)};
         success = ImGui::Button("Set Rule") || success;
